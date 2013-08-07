20125459eac55d430631b6fd68441e478a46ba68
Hashcode <hashcode0f@gmail.com>
2013-08-07 14:46:39 -0700

    fix boot.mk for phablet-saucy build

--- boot.mk
+++ boot.mk
@@ -15,34 +15,45 @@
 DEVICE_FOLDER := device/amazon/otter2
 COMMON_FOLDER := device/amazon/omap4-common
 
-OTTER2_BOOTLOADER := $(DEVICE_FOLDER)/prebuilt/boot/u-boot.bin
-OTTER2_BOOT_CERT_FILE := $(DEVICE_FOLDER)/prebuilt/boot/boot_cert
-OTTER2_BOOT_ADDRESS := '\x00\x50\x7c\x80'
-OTTER2_STACK_FILE := /tmp/stack.tmp
+BOWSER_BOOTLOADER := $(DEVICE_FOLDER)/prebuilt/boot/u-boot.bin
+BOWSER_BOOT_CERT_FILE := $(DEVICE_FOLDER)/prebuilt/boot/boot_cert
+BOWSER_BOOT_ADDRESS := '\x00\x50\x7c\x80'
+BOWSER_STACK_FILE := /tmp/stack.tmp
 
 define make_stack
-  for i in $$(seq 1024) ; do echo -ne $(OTTER2_BOOT_ADDRESS) >>$(1) ; done
+  for i in $$(seq 1024) ; do echo -ne $(BOWSER_BOOT_ADDRESS) >>$(1) ; done
 endef
 
-INTERNAL_BOOTIMAGE_ARGS2 := \
-	--kernel $(INSTALLED_KERNEL_TARGET) \
-	--ramdisk $(COMMON_FOLDER)/initrd.img-touch \
-	--cmdline "$(BOARD_KERNEL_CMDLINE)" \
-	--base $(BOARD_KERNEL_BASE) \
-	--pagesize $(BOARD_KERNEL_PAGESIZE)
+INTERNAL_BOOTIMAGE_ARGS += $(INTERNAL_BOOTIMAGE_ARGS_COMMON)
+INTERNAL_BOOTIMAGE_ARGS_ANDROID += $(INTERNAL_BOOTIMAGE_ARGS_COMMON)
 
-$(INSTALLED_BOOTIMAGE_TARGET): \
-		$(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(OTTER2_BOOTLOADER)
-	$(call pretty,"Making target Ubuntu-Touch boot image: $@")
-	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS2) --output $@.tmp
-	$(hide) cat $(OTTER2_BOOT_CERT_FILE) $@.tmp >$@
+$(INSTALLED_BOOTIMAGE_TARGET_ANDROID): \
+		$(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES_ANDROID) $(BOWSER_BOOTLOADER)
+	$(call pretty,"Target boot image for Android: $@")
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS_ANDROID) $(BOARD_MKBOOTIMG_ARGS) --output $@.tmp
+	$(hide) cat $(BOWSER_BOOT_CERT_FILE) $@.tmp >$@
 	$(hide) rm -f $@.tmp
 	$(call pretty,"Adding kindle specific u-boot for boot.img")
-	$(hide) dd if=$(OTTER2_BOOTLOADER) of=$@ bs=8117072 seek=1 conv=notrunc
+	$(hide) dd if=$(BOWSER_BOOTLOADER) of=$@ bs=8117072 seek=1 conv=notrunc
 	$(call pretty,"Adding kindle specific payload to boot.img")
-	$(call make_stack,$(OTTER2_STACK_FILE))
-	$(hide) dd if=$(OTTER2_STACK_FILE) of=$@ bs=6519488 seek=1 conv=notrunc
-	$(hide) rm -f $(OTTER2_STACK_FILE)
+	$(call make_stack,$(BOWSER_STACK_FILE))
+	$(hide) dd if=$(BOWSER_STACK_FILE) of=$@ bs=6519488 seek=1 conv=notrunc
+	$(hide) rm -f $(BOWSER_STACK_FILE)
+	@echo -e ${CL_CYN}"Made boot image for Android: $@"${CL_RST}
+
+$(INSTALLED_BOOTIMAGE_TARGET): \
+		$(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOWSER_BOOTLOADER)
+	$(call pretty,"Target boot image for Ubuntu Touch: $@")
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@.tmp2
+	$(hide) cat $(BOWSER_BOOT_CERT_FILE) $@.tmp2 >$@
+	$(hide) rm -f $@.tmp
+	$(call pretty,"Adding kindle specific u-boot for boot.img")
+	$(hide) dd if=$(BOWSER_BOOTLOADER) of=$@ bs=8117072 seek=1 conv=notrunc
+	$(call pretty,"Adding kindle specific payload to boot.img")
+	$(call make_stack,$(BOWSER_STACK_FILE))
+	$(hide) dd if=$(BOWSER_STACK_FILE) of=$@ bs=6519488 seek=1 conv=notrunc
+	$(hide) rm -f $(BOWSER_STACK_FILE)
+	@echo -e ${CL_CYN}"Made boot image for Ubuntu Touch: $@"${CL_RST}
 
 # XXX - While I'd like to do this check, currently it adds some reserve
 # that totally kills the deal.
@@ -50,16 +61,12 @@ $(INSTALLED_BOOTIMAGE_TARGET): \
 #		$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): \
-		$(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel) \
-		$(OTTER2_BOOTLOADER)
-	@echo ----- Making recovery image ------
-	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) --output $@.tmp
-	$(hide) cat $(OTTER2_BOOT_CERT_FILE) $@.tmp >$@
+		$(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel) $(BOWSER_BOOTLOADER)
+	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
+	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@.tmp
+	$(hide) cat $(BOWSER_BOOT_CERT_FILE) $@.tmp >$@
 	$(hide) rm -f $@.tmp
 	$(call pretty,"Adding kindle specific u-boot for recovery.img")
-	$(hide) dd if=$(OTTER2_BOOTLOADER) of=$@ bs=8117072 seek=1 conv=notrunc
-# XXX - While I'd like to do this check, currently it adds some reserve
-# that totally kills the deal.
-#	$(hide) $(call assert-max-image-size,$@,\
-#		$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+	$(hide) dd if=$(BOWSER_BOOTLOADER) of=$@ bs=8117072 seek=1 conv=notrunc
+	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
 
